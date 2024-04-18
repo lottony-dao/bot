cf13987921c4b93c675aa860f675b607212a8d11
gfavaz <gabashidze99@gmal.com>
2024-04-18 15:39:50 +0400

    улучшено система try catch для полного контроля работы и исключения падения бота. также добавлено логирование ошибок.

--- dao_bot/src/index.ts
+++ dao_bot/src/index.ts
@@ -1,11 +1,14 @@
 import { Bot, Context, GrammyError, HttpError } from 'grammy';
 import { handleMessage } from '~/chat';
 import { checkUserActivity } from './chat/public/checkUserActivity';
+import * as process from "node:process";
 
 require('dotenv').config();
 
 const chat_id = process.env.MAIN_CHAT_ID;
 const token = process.env.BOT_TOKEN;
+const defaultRetryLimit = 1;
+const retry_limit = parseInt(process.env.RETRY_LIMIT || '', 10) || defaultRetryLimit;
 
 if (!token) {
     throw new Error('Declare BOT_TOKEN in .env file in the root of the project');
@@ -13,43 +16,51 @@ if (!token) {
 
 const bot = new Bot(token);
 
-async function sendMessageWithRetry(ctx: Context, text: string, retryLimit: number = 10) {
-    for (let i = 0; i < retryLimit; i++) {
+async function sendMessageWithRetry(ctx: Context, text: string, retryLimit: number = retry_limit) {
+    let attempt = 0;
+    while (attempt < retryLimit) {
         try {
             await ctx.reply(text);
-            break; // Успешная отправка, выходим из цикла
+            return;
         } catch (error) {
+            attempt++;
             if (error instanceof GrammyError && error.error_code === 429) {
-                const delay = error.parameters?.retry_after ? error.parameters.retry_after * 1000 : 1000; // Используйте значение по умолчанию, если retry_after не задан
-                console.log(`Too many requests, retrying after ${delay} ms`);
+                const delay = error.parameters?.retry_after ? error.parameters.retry_after * 1000 : 1000;
+                console.log(`Attempt ${attempt}: Too many requests, retrying after ${delay} ms`);
                 await new Promise(resolve => setTimeout(resolve, delay));
             } else {
-                console.error('Failed to send message:', error);
-                throw error;
+                console.error(`Attempt ${attempt}: Failed to send message:`, error);
+                if (attempt >= retryLimit) {
+                    console.log("Max retry attempts reached, giving up.");
+                    throw new Error("Failed to send message after retries.");
+                }
             }
         }
     }
 }
 
 bot.on('message', async (ctx: Context) => {
-    let chat = ctx.chat || null;
+    try {
+        let chat = ctx.chat || null;
+        if (!chat) return;
 
-    if (!chat) {
-        return;
-    }
-    if (chat.type === 'private') {
-        if (ctx.message?.text) {
-            // Обработка личных сообщений здесь, если требуется
+        if (chat.type === 'private' && ctx.message?.text) {
+            // Обработка личных сообщений
+        } else if (chat_id && chat.id.toString() === chat_id) {
+            const activityCheck = await checkUserActivity(ctx);
+            if (!activityCheck) {
+                handleMessage(ctx);
+            } else {
+                const username = ctx.from?.username || ctx.from?.first_name || ctx.from?.id;
+                try {
+                    await sendMessageWithRetry(ctx, `${username} - не надо жульничать, сегодня ты уже баловался этим.`);
+                } catch (retryError) {
+                    console.error("Error after all retries:", retryError);
+                }
+            }
         }
-    } else if (chat_id && (chat.id.toString() === chat_id)) {
-        const activityCheck = await checkUserActivity(ctx);
-        if (!activityCheck) {
-            handleMessage(ctx);
-        } else {
-            const username = ctx.from?.username || ctx.from?.first_name || ctx.from?.id;
-            // Прямая отправка сообщения без использования очереди
-            await sendMessageWithRetry(ctx, `${username} - не надо жульничать, сегодня ты уже баловался этим.`);
-        }
+    } catch (error) {
+        console.error('Error handling message:', error);
     }
 });
 
